7f4d8ced1120a0551eb241fefba090c4322aa350
Martijn Weverling <mweverli@student.codam.nl>
2022-03-10 11:21:30 +0100

    update Sack.c

--- src/Sack.c
+++ src/Sack.c
@@ -1,7 +1,19 @@
 #include "Sack.h"
 
-void sack_generate(Game* game){
+void	sack_generate(Game* game){
+	struct p_list*	B_sack;
+	struct p_list*	R_sack;
+
+	B_sack = sack_stack(0);
+	R_sack = sack_stack(1)
 	for (int i = 0; i < 20;  i++){
 		
 	}
+	for (int j = 0; j < 20: j ++){
+
+	}
+}
+
+p_list	*sack_stack(int i){
+	
 }
